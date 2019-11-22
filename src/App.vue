de4ea32ca61ddb703549707f1a6dbc80e0fb989c
kahan <kahan@d17-l427-pc-01.ipa.ki.agh.edu.pl>
2019-11-22 17:48:10 +0100

    Initial Vue.js project

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <ul v-if="numbers.length> 0">
+      <li v-for="number in numbers">{{number}}</li>
+      </ul>
+  <h1>Sign in</h1>
+  <button @click="addNew()"> Or let's sign up</button>
+
+  <input type="text" v-model="title">
+  <input type="text" v-model="theme">
+
+    <img alt="Vue logo" src="./assets/logo.png">
+    
+  
+
+
+
+<button @click="iAmClicked()">Sign in</button>
+</div>
+</template>
+
+<script>
+export default {
+        data() {
+            return {numbers:[],title:"Name:",theme:"Password:"}
+        },
+        methods: {
+            iAmClicked() {
+                this.title = 'Clicked';
+            },
+            addNew(){
+              this.theme = 'cos';
+            }
+        }
+}
+        
+
+;
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Comic, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: blue;
+  text-align: center;
+  color: #2c3e59;
+  margin-top: 120px;
+}
+</style>
